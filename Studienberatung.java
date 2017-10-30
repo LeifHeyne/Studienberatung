9df6e6ccbf40cedda5929994ebab58b1b635796c
LeifHeyne <Leif.Heyne@gmx.de>
2017-10-30 21:49:53 +0100

    hast-du-fachabiur

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,8 +3,15 @@ import java.util.*;
 public class Studienberatung{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
-		System.out.print("Hast du Abitur?");
+		System.out.println("Hast du Abitur?");
 		String ans = s.next();
+		
+		if(ans.equalsIgnoreCase("n")){
+			//Code für kein Abitur
+		}
+		else{
+			
+		}
 		
 	}
 }
