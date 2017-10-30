5c9f36be70da2f7fac4cf60db5a1c08774a63fd1
LeifHeyne <Leif.Heyne@gmx.de>
2017-10-30 19:23:11 +0100

    main Methode hinzugefügt/ Scanner hinzugefügt/ Hast du Abitur

---
+++ Studienberatung.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Studienberatung{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		System.out.print("Hast du Abitur?");
+		String ans = s.next();
+		
+	}
+}
