24612d6814e13d728f89bd0a7890f77cab2a09c6
LeifHeyne <Leif.Heyne@gmx.de>
2017-10-30 23:00:20 +0100

    finish

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,8 +3,43 @@ import java.util.*;
 public class Studienberatung{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
-		System.out.print("Hast du Abitur?");
+		System.out.println("Hast du Abitur?");
 		String ans = s.next();
+		
+
+		if(ans.equalsIgnoreCase("n")){
+			System.out.println("Hast du Fachabitur?");
+			 ans = s.next();
+			if(ans.equalsIgnoreCase("j")){
+				System.out.println("Hast du eine Berufsausbildung?");
+				ans = s.next();
+				if(ans.equalsIgnoreCase("n")){
+					System.out.println("Besser noch Studieren");
+					
+				}
+				else if ( ans.equalsIgnoreCase("j")){
+					System.out.println("Du kannst ja später noch Studieren");
+				}
+			}
+			else if(ans.equalsIgnoreCase("n")){
+				System.out.println("Nicht Studieren");
+			}
+		}
+
+		else if( ans.equalsIgnoreCase("j")){
+			System.out.println("Bist du wissbegierig?");
+			 ans = s.next();
+			if(ans.equalsIgnoreCase("j")){
+				System.out.println("Auf jeden Fall studieren!");
+			}
+			else if(ans.equalsIgnoreCase("n")){
+				System.out.println("Nicht Studieren");
+			}
+
+		}
+		else{
+			
+		}
 		
 	}
 }
